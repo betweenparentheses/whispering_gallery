dfa530701af18d02307a7b43cd1c68d01b0b730e
Michael Alexander <alexandermw@gmail.com>
2015-05-14 01:36:26 -0700

    expired key retirement added

--- models/message_cache.rb
+++ models/message_cache.rb
@@ -20,12 +20,17 @@ class MessageCache
   end
 
   def add_message(text)
+    delete_expired
     @redis.zadd "messages",
               new_expiration_score,
               text
   end
 
   private
+
+  def delete_expired
+    @redis.zremrangebyscore "messages", 0, now
+  end
 
   def now
     Time.now.utc.to_i
@@ -37,7 +42,7 @@ class MessageCache
 
   # one hour expiration right now
   def new_expiration_score
-    (Time.now + 3600).utc.to_i
+    now + 3600
   end
 
 end
