11718b920398c76b333ea671c4c61ba91af3a0c1
Michael Alexander <alexandermw@gmail.com>
2015-05-16 09:12:56 -0700

    changes for heroku

--- app.rb
+++ app.rb
@@ -25,10 +25,17 @@ end
 
 
 set(:watcher, Thread.new do
-  redis = Redis.new
-  puts redis.inspect
-  puts redis.host
-  puts redis.port
+
+  heroku_redis = ENV["REDISCLOUD_URL"]
+  if heroku_redis
+    uri = URI.parse(heroku_redis)
+    redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+  else
+    redis = Redis.new
+  end
+
+
+
   Thread.current['sockets'] = []
 
   redis.subscribe 'whispers' do |on|
@@ -42,25 +49,29 @@ end)
 
 
 get '/whisper' do
-  cache = MessageCache.new($redis)
-  request.websocket do |ws|
-    ws.onopen do
-      ws.send("We're whispering!")
-      settings.watcher['sockets'] << ws
-    end
-    ws.onmessage do |msg|
-      if msg == "/refresh/"
-        $redis.publish 'whispers', cache.poll_messages
-      else
-        $redis.publish 'whispers', "/sent/"
-        cache = MessageCache.new($redis)
-        cache.add_message( msg )
+  if request.websocket?
+    cache = MessageCache.new($redis)
+    request.websocket do |ws|
+      ws.onopen do
+        ws.send("We're whispering!")
+        settings.watcher['sockets'] << ws
+      end
+      ws.onmessage do |msg|
+        if msg == "/refresh/"
+          $redis.publish 'whispers', cache.poll_messages
+        else
+          $redis.publish 'whispers', "/sent/"
+          cache = MessageCache.new($redis)
+          cache.add_message( msg )
+        end
+      end
+      ws.onclose do
+        warn('websocket has been closed')
+        settings.watcher['sockets'].delete(ws)
       end
     end
-    ws.onclose do
-      warn('websocket has been closed')
-      settings.watcher['sockets'].delete(ws)
-    end
+  else
+    render 401
   end
 end
 
